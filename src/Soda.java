695056042f72070593881f316135129865d368da
Liliia Baieva <555blv@gmail.com>
2023-03-08 19:57:50 +0100

    add String GetMyDrinkString() method

--- src/Soda.java
+++ src/Soda.java
@@ -8,19 +8,20 @@ public class Soda {
   public Soda(String additive){
     this.additive = additive;
   }
-  }
-
-
 
   public Soda(){
     this.additive = "Обычная газировака";
   }
 
-
-
-
-
-
+  //  В этом классе реализуйте метод public String GetMyDrinkString(),
+  //  возвращающий строку Газировка и {добавка} в случае наличия добавки.
+  //    Если добавки нет, нужно вернуть строку "Обычная газировка".
+  public String GetMyDrinkString(){
+    if(additive.contains("Обычная газировака")){
+      return additive;
+    }
+    return String.format("Газировка и %s", additive);
+  }
 
 }
 
